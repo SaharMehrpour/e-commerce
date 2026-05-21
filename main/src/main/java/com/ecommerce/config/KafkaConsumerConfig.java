dfdade773137a6cb2c2c84e1880587d524443140
saharmehrpour <sahar.mehrpour@gmail.com>
2026-05-20 19:57:23 -0500

    Kafka consumer is added

---
+++ main/src/main/java/com/ecommerce/config/KafkaConsumerConfig.java
@@ -0,0 +1,100 @@
+package com.ecommerce.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+import org.springframework.util.backoff.FixedBackOff;
+
+import com.ecommerce.event.OrderCancelledEvent;
+import com.ecommerce.event.OrderCreatedEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, OrderCreatedEvent> createdConsumerFactory() {
+
+        JacksonJsonDeserializer<OrderCreatedEvent> deserializer =
+                        new JacksonJsonDeserializer<>(OrderCreatedEvent.class);
+
+        deserializer.addTrustedPackages("com.ecommerce.event");
+        deserializer.setUseTypeHeaders(false);
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "ecommerce-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent>
+    orderCreatedKafkaListenerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(createdConsumerFactory());
+        factory.setCommonErrorHandler(errorHandler());
+
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, OrderCancelledEvent> cancelledConsumerFactory() {
+
+        JacksonJsonDeserializer<OrderCancelledEvent> deserializer =
+                new JacksonJsonDeserializer<>(OrderCancelledEvent.class);
+
+        deserializer.addTrustedPackages("com.ecommerce.event");
+        deserializer.setUseTypeHeaders(false);
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "ecommerce-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, OrderCancelledEvent>
+    orderCancelledKafkaListenerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, OrderCancelledEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(cancelledConsumerFactory());
+        factory.setCommonErrorHandler(errorHandler());
+
+        return factory;
+    }
+
+    @Bean
+    public DefaultErrorHandler errorHandler() {
+        FixedBackOff backOff = new FixedBackOff(1000L, 3);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler((record, exception) -> {
+            System.err.println("Skipping message due to error: " + exception.getMessage());
+        }, backOff);
+        
+        return errorHandler;
+    }
+}

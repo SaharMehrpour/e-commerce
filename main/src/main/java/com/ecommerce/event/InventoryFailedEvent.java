acdaa86025af318b759c2914377c2231b40906db
saharmehrpour <sahar.mehrpour@gmail.com>
2026-06-04 15:30:56 -0500

    kafka events emit in Inventory Service

--- main/src/main/java/com/ecommerce/event/InventoryFailedEvent.java
+++ main/src/main/java/com/ecommerce/event/InventoryFailedEvent.java
@@ -8,11 +8,15 @@ import lombok.NoArgsConstructor;
 public class InventoryFailedEvent extends Event {
 
     private String orderId;
+    private String productId;
+    private Integer quantity;
+            
+    private String failedAction;
     private String failureReason;
 
     public InventoryFailedEvent(
             String eventId,
-            String orderId,
+            String failedAction,
             String failureReason
     ) {
         super(
@@ -21,12 +25,36 @@ public class InventoryFailedEvent extends Event {
                 Instant.now().toString()
         );
 
-        this.orderId = orderId;
+        this.failedAction = failedAction;
         this.failureReason = failureReason;
     }
 
     public String getOrderId() {
         return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    
+    public String getFailedAction() {
+        return failedAction;
     }
 
     public String getFailureReason() {

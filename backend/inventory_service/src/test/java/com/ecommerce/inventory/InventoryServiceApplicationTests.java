773c7712c86483cd8699806918ea339398b1bfd8
saharmehrpour <sahar.mehrpour@gmail.com>
2026-06-10 16:32:12 -0500

    added kafka containers for tests

--- backend/inventory_service/src/test/java/com/ecommerce/inventory/InventoryServiceApplicationTests.java
+++ backend/inventory_service/src/test/java/com/ecommerce/inventory/InventoryServiceApplicationTests.java
@@ -2,9 +2,18 @@ package com.ecommerce.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class InventoryServiceApplicationTests {
+
+    @Container
+    static ConfluentKafkaContainer kafka = new ConfluentKafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:7.5.0"));
 
     @Test
     void contextLoads() {
